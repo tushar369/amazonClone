7ff61ba66805e4b29db762b3c49972af1155082c
tushar <tusharyadav34@gmail.com>
2021-09-10 11:43:20 +0530

    reactWrappendWorking

---
+++ frontend/src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const App = () => {
+  return <div>This is app working. hello</div>;
+};
+
+export default App;
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,33 +1,30 @@
 const path = require("path");
 const webpack = require("webpack");
 
-module.export = {
+module.exports = {
   entry: "./src/index.js",
-
   output: {
-    path: path.resolve(__dirname, "./static/js"),
+    path: path.resolve(__dirname, "./static/frontend"),
     filename: "[name].js",
   },
-
   module: {
     rules: [
       {
         test: /\.js$/,
-        exclude: /node_nodules/,
+        exclude: /node_modules/,
         use: {
           loader: "babel-loader",
         },
       },
     ],
   },
-
   optimization: {
     minimize: true,
   },
-
   plugins: [
     new webpack.DefinePlugin({
       "process.env": {
+        // This has effect on the react lib size
         NODE_ENV: JSON.stringify("development"),
       },
     }),
